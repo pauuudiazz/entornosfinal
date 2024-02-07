5117650529f154bce5f029b672a5efe4fef202f4
pauuudiazz <157788917+pauuudiazz@users.noreply.github.com>
2024-02-07 10:33:24 +0100

    codigo

---
+++ src/main/java/paulaa/entornosfinal2/entonosfinal.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package paulaa.entornosfinal2;
+
+/**
+ *
+ * @author AlumnoMañana
+ */
+public class entonosfinal {
+
+    public static void main(String[] args) {
+       int k = 0;
+do {
+System.out.println("Iteración do-while: " + k);
+k++;
+} while (k < 5 );
+// switch con keyword default
+int dia = 4;
+switch (dia) {
+case 6:
+System.out.println("Hoy es Sabado");
+break;
+case 7:
+System.out.println("Hoy es Domingo");
+break;
+default:
+System.out.println("Esperando el fin de semana");
+}
+        
+
+    }
+
+}
